498f312b755e9d88965c49ce17b9bdb2f1cf8d0e
Michael Kurz <michael.kurz@irian.at>
2013-06-18 21:50:21 +0200

    added new example for JSF 2.2 resetValues

---
+++ jsf22-reset-values/src/main/java/at/jsflive/jsf22/TestBean.java
@@ -0,0 +1,34 @@
+package at.jsflive.jsf22;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * @author Michael Kurz
+ */
+@ManagedBean
+@SessionScoped
+public class TestBean {
+    private int value1;
+    private int value2;
+
+    public void increaseValue1() {
+        value1++;
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+
+    public void setValue1(int value1) {
+        this.value1 = value1;
+    }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
+}
