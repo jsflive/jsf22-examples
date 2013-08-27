cdd81d2e3ecf9d4d4fdd254b2e72aded86bcb553
Michael Kurz <michi.kurz@gmx.at>
2013-08-27 22:02:30 +0200

    added example jsf22-flows

---
+++ jsf22-flows/src/main/java/at/jsflive/jsf22/example01/PersonPage.java
@@ -0,0 +1,46 @@
+package at.jsflive.jsf22.example01;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class PersonPage {
+    @Inject
+    private PersonRepository personRepository;
+
+    private int selectedId;
+    private Person selectedPerson;
+
+    public List<Person> getPersons() {
+        return personRepository.getPersons();
+    }
+
+    public void loadPerson() {
+        selectedPerson = personRepository.getPerson(selectedId);
+    }
+
+    public void addInfoMessage() {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        String msg = "View action " + (ctx.isPostback() ? "on postback" : "on initial request");
+        ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+        ctx.getExternalContext().getFlash().setKeepMessages(true);
+    }
+
+    public Person getSelectedPerson() {
+        return selectedPerson;
+    }
+
+    public void setSelectedId(int selectedId) {
+        this.selectedId = selectedId;
+    }
+
+    public int getSelectedId() {
+        return selectedId;
+    }
+
+}
