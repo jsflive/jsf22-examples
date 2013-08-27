600582e95cb64f0febfcbb9cf70f63cd0647013d
Michael Kurz <michi.kurz@gmx.at>
2013-08-27 22:36:41 +0200

    added flows to flows example

---
+++ jsf22-flows/src/main/java/at/jsflive/jsf22/flows/Flow3Bean.java
@@ -0,0 +1,28 @@
+package at.jsflive.jsf22.flows;
+
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+@Named
+@FlowScoped(value = "flow3")
+public class Flow3Bean {
+    private String value1;
+    private String value2;
+
+    public String getValue1() {
+        return value1;
+    }
+
+    public void setValue1(String value1) {
+        this.value1 = value1;
+    }
+
+    public String getValue2() {
+        return value2;
+    }
+
+    public void setValue2(String value2) {
+        this.value2 = value2;
+    }
+
+}
